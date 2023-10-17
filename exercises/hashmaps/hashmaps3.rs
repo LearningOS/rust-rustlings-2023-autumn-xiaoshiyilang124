64e4a55f7a94133d2e94b628f1d3d82b09a4f4ff
jim <jimschmitt@qq.com>
2023-10-17 19:00:12 +0800

    hashmap1

--- exercises/hashmaps/hashmaps3.rs
+++ exercises/hashmaps/hashmaps3.rs
@@ -34,6 +34,12 @@ fn build_scores_table(results: String) -> HashMap<String, Team> {
         let team_1_score: u8 = v[2].parse().unwrap();
         let team_2_name = v[1].to_string();
         let team_2_score: u8 = v[3].parse().unwrap();
+        if !scores.contains_key(&team_1_name){
+            scores.insert(team_1_name,Team{goals_scored:team_1_score,goals_scored:team_2_score});
+        }
+        if !scores.contains_key(&team_2_name){
+            scores.insert(team_2_name,Team{goals_scored:team_2_score,goals_scored:team_1_score});
+        }
         // TODO: Populate the scores table with details extracted from the
         // current line. Keep in mind that goals scored by team_1
         // will be the number of goals conceded from team_2, and similarly
@@ -83,4 +89,8 @@ mod tests {
         assert_eq!(team.goals_scored, 0);
         assert_eq!(team.goals_conceded, 2);
     }
+}
+
+fn main(){
+
 }
