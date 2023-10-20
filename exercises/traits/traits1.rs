09f1686a9175f7bff0efd09f9d326e925e24d0f1
jim <jimschmitt@qq.com>
2023-10-20 19:19:25 +0800

    trait1

--- exercises/traits/traits1.rs
+++ exercises/traits/traits1.rs
@@ -4,10 +4,8 @@
 // `AppendBar` for the type `String`. The trait AppendBar has only one function,
 // which appends "Bar" to any object implementing this trait.
 //
-// Execute `rustlings hint traits1` or use the `hint` watch subcommand for a
-// hint.
+// Execute `rustlings hint traits1` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
 
 trait AppendBar {
     fn append_bar(self) -> Self;
@@ -15,6 +13,9 @@ trait AppendBar {
 
 impl AppendBar for String {
     // TODO: Implement `AppendBar` for type `String`.
+    fn append_bar(self) -> Self {
+        self + "Bar"
+    }
 }
 
 fn main() {
