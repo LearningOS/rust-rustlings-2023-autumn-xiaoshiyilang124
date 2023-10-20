0e5451c730cbf8d96c42ea608c3283e20521353d
jim <jimschmitt@qq.com>
2023-10-20 19:51:38 +0800

    quizs3

--- exercises/tests/tests1.rs
+++ exercises/tests/tests1.rs
@@ -7,15 +7,13 @@
 // This test has a problem with it -- make the test compile! Make the test pass!
 // Make the test fail!
 //
-// Execute `rustlings hint tests1` or use the `hint` watch subcommand for a
-// hint.
+// Execute `rustlings hint tests1` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
 
 #[cfg(test)]
 mod tests {
     #[test]
     fn you_can_assert() {
-        assert!();
+        assert!(true);
     }
 }
