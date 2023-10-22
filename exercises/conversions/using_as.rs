7bf34aecc85db918cc83cc9f55a46d965f6ca588
jim <jimschmitt@qq.com>
2023-10-22 14:07:39 +0800

    using_as

--- exercises/conversions/using_as.rs
+++ exercises/conversions/using_as.rs
@@ -7,14 +7,15 @@
 // The goal is to make sure that the division does not fail to compile and
 // returns the proper type.
 //
-// Execute `rustlings hint using_as` or use the `hint` watch subcommand for a
-// hint.
+// Execute `rustlings hint using_as` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
+
+use std::f64;
 
 fn average(values: &[f64]) -> f64 {
     let total = values.iter().sum::<f64>();
-    total / values.len()
+    let length = values.len() as f64;
+    total / length
 }
 
 fn main() {
